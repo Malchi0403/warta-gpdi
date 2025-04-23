21f71e4b654311cf6c34b75152e3fd5c942661ce
Malchiram <storeflash04@gmail.com>
2025-04-23 21:25:40 +0700

    add jsonld

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import ReduxProvider from "@/libs/provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,16 @@ export const metadata: Metadata = {
     "Selamat datang di Warta Jemaat GPdI Shekinah Mustika Jaya",
   keywords: [
     "GPdI Shekinah",
+    "gpdi",
+    "shekinah",
+    "gereja mustika jaya",
+    "mustika jaya",
+    "ibadah minggu",
+    "praise and worship",
+    "praise",
+    "worship",
+    "pantekosta",
+    "pantekosta",
     "gereja",
     "ibadah",
     "kasih",
@@ -52,30 +63,36 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
-  // const jsonLd = {
-  //   "@context": "https://schema.org",
-  //   "@type": "Church",
-  //   "name": "GPdI Shekinah",
-  //   "address": {
-  //     "@type": "PostalAddress",
-  //     "streetAddress": "Graha Harapan Blok E 13 no 2 , Mustika Jaya",
-  //     "addressLocality": "Kota Bekasi",
-  //     "addressRegion": "Jawa Barat",
-  //     "postalCode": "17158",
-  //     "addressCountry": "ID"
-  //   },
-  //   "telephone": "+62 813-1614-5742",
-  //   "url": "https://www.gpdishekinah.online",
-  //   "openingHours": "Mo-Su 00:00-23:59",
-  //   "description": "GPdI Shekinah adalah gereja yang berlokasi di Mustika jaya, dengan ibadah setiap hari Minggu dan dihari lain ada beberapa kegiatan-kegiatan gereja.",
-  //   "geo": {
-  //     "@type": "GeoCoordinates",
-  //     "latitude": "-6.303093633833016",
-  //     "longitude": " 107.02616911398482"
-  //   }
-  // }
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Church",
+    "name": "GPdI Shekinah",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "Graha Harapan Blok E 13 no 2 , Mustika Jaya",
+      "addressLocality": "Kota Bekasi",
+      "addressRegion": "Jawa Barat",
+      "postalCode": "17158",
+      "addressCountry": "ID"
+    },
+    "telephone": "+62 813-1614-5742",
+    "url": "https://www.gpdishekinah.online",
+    "openingHours": "Mo-Su 00:00-23:59",
+    "description": "GPdI Shekinah adalah gereja yang berlokasi di Mustika jaya, dengan ibadah setiap hari Minggu dan dihari lain ada beberapa kegiatan-kegiatan gereja.",
+    "geo": {
+      "@type": "GeoCoordinates",
+      "latitude": "-6.303093633833016",
+      "longitude": " 107.02616911398482"
+    }
+  }
   return (
     <html lang="en">
+      <Head>
+      <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </Head>
       <body
         suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
